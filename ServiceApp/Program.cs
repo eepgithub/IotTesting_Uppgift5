19a5259177dc722f739c47b48c7b7fdf6d2c9d7a
Emil <emilpppe@gmail.com>
2020-09-24 18:26:26 +0200

    Add project files.

---
+++ ServiceApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=EC-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=wCIx9CZhxPRw9ZAEredokQ7Z5Qm/F4YLWPpn1eWAYMw=");
+
+        static void Main(string[] args)
+        {
+            Task.Delay(5000).Wait();
+
+
+            InvokeMethod("DeviceApp", "SetTelemetryInterval", "10").GetAwaiter();
+            Console.ReadKey();
+        }
+
+
+        static async Task InvokeMethod(string deviceID, string methodName, string payload)
+        {
+            var methodInvocation = new CloudToDeviceMethod(methodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson(payload);
+
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(deviceID, methodInvocation);
+            Console.WriteLine($"Response Status: {response.Status}");
+            Console.WriteLine(response.GetPayloadAsJson());
+
+        }
+
+
+
+
+    }
+}
