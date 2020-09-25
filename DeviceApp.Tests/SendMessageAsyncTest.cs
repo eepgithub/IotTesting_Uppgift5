43a8d755b9e44410bfd663c6c71c6a3ad184228d
Emil <emilpppe@gmail.com>
2020-09-25 13:06:34 +0200

    G klar

---
+++ DeviceApp.Tests/SendMessageAsyncTest.cs
@@ -0,0 +1,36 @@
+using Newtonsoft.Json;
+using System;
+using Xunit;
+using Xunit.Sdk;
+
+namespace DeviceApp.Tests
+{
+    public class SendMessageAsyncTest
+    {
+        [Fact]
+        public void SendMessageAsync_ShouldConvertToJson()
+        {
+
+
+            TemperatureModel json = new TemperatureModel
+            {
+                Temperature = 10,
+                Humidity = 20
+
+
+            };
+
+            string expected = "{\"Temperature\":10,\"Humidity\":20}";
+            string actual = JsonConvert.SerializeObject(json);
+            Assert.Equal(expected, actual);
+
+
+        }
+
+    public class TemperatureModel
+    {
+        public int Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
+}
